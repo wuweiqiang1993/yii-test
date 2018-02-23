444557ff9a3cf5a58e784a1087f81f92fe78b442
wuweiqiang1993 <wuweiqiang1993@github.com>
2018-02-23 22:20:11 +0800

    初始化

---
+++ views/site/entry.php
@@ -0,0 +1,15 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+<?php $form = ActiveForm::begin(); ?>
+
+<?= $form->field($model, 'name') ?>
+
+<?= $form->field($model, 'email') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('提交', ['class' => 'btn btn-warning']) ?>
+    </div>
+
+<?php ActiveForm::end(); ?>
---
+++ views/site/say.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/30
+ * Time: 22:49
+ */
+use yii\helpers\Html;
+?>
+<?= Html::encode($message) ?>
